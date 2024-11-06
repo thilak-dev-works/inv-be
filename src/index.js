ba4ef5666af0a8fbc58cf4a46a33a3cafa660019
Thilak <deepa_thilakraj@protonmail.com>
2024-11-06 20:26:27 +0530

    inv

--- src/index.js
+++ src/index.js
@@ -1,43 +1,24 @@
-/* eslint-disable no-undef */
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 
-const uri = config.mongoose.url; // Use environment variable for URI
+let isConnected; // Track MongoDB connection
 
-mongoose
-  .connect(uri, config.mongoose.options)
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
-// Export the app for serverless deployment
-module.exports = app;
-
-// Graceful shutdown handlers
-const exitHandler = () => {
-  if (server) {
-    server.close(() => {
-      logger.info('Server closed');
-      process.exit(1);
-    });
-  } else {
-    process.exit(1);
+async function connectToDatabase() {
+  if (isConnected) {
+    return;
   }
-};
+  await mongoose.connect(process.env.MONGODB_URI, config.mongoose.options);
+  isConnected = mongoose.connection.readyState === 1; // Set to true if connected
+  console.log('Connected to MongoDB');
+}
 
-const unexpectedErrorHandler = (error) => {
-  logger.error(error);
-  exitHandler();
-};
-
-process.on('uncaughtException', unexpectedErrorHandler);
-process.on('unhandledRejection', unexpectedErrorHandler);
-
-process.on('SIGTERM', () => {
-  logger.info('SIGTERM received');
-  if (server) {
-    server.close();
-  }
+// Middleware to ensure MongoDB connection is established
+app.use(async (req, res, next) => {
+  await connectToDatabase();
+  next();
 });
+
+module.exports = app;
