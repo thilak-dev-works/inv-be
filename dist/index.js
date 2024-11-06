05073e3cb98b0b7dd9cd8d6905a4552279cdae31
Thilak <deepa_thilakraj@protonmail.com>
2024-11-06 20:06:57 +0530

    inv

--- dist/index.js
+++ dist/index.js
@@ -3,12 +3,22 @@ const app = require('./app');
 const config = require('./config/config');
 const logger = require('./config/logger');
 let server;
-mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
-  logger.info('Connected to MongoDB');
-  server = app.listen(8000, () => {
-    logger.info(`Listening to port ${80}`);
+const uri = 'mongodb+srv://thilaksubramani17:kNmNuGsQ9OfvisQ7@thilak17.boal2.mongodb.net/?authSource=admin&replicaSet=atlas-gvvwy9-shard-0&retryWrites=true&w=majority&appName=thilak17%2Fbestingems';
+
+// Only start the server if running locally
+if (process.env.NODE_ENV !== 'production') {
+  mongoose.connect(uri, config.mongoose.options).then(() => {
+    logger.info('Connected to MongoDB');
+    server = app.listen(8000, () => {
+      logger.info('Listening to port 8000');
+    });
   });
-});
+}
+
+// Export the app for Vercel
+module.exports = app;
+
+// Graceful shutdown handlers
 const exitHandler = () => {
   if (server) {
     server.close(() => {
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ const config = require('./config/config');
 const logger = require('./config/logger');
 
 let server;
+const uri =
+  'mongodb+srv://thilaksubramani17:kNmNuGsQ9OfvisQ7@thilak17.boal2.mongodb.net/?authSource=admin&replicaSet=atlas-gvvwy9-shard-0&retryWrites=true&w=majority&appName=thilak17%2Fbestingems';
 
 // Only start the server if running locally
 if (process.env.NODE_ENV !== 'production') {
-  mongoose.connect(config.mongoose.url, config.mongoose.options).then(() => {
+  mongoose.connect(uri, config.mongoose.options).then(() => {
     logger.info('Connected to MongoDB');
     server = app.listen(8000, () => {
-      logger.info(`Listening to port 8000`);
+      logger.info('Listening to port 8000');
     });
   });
 }
